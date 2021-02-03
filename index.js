445307ec5b6e2b7d9edacb1594c19bb7b15c0e6b
George An <carbongo@hotmail.com>
2021-02-03 17:31:02 +0900

    🧶 WIP: locales done, game logic done

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 #!/usr/bin/env node
 const readline = require('readline');
 const english = require('./locale/en.json');
+const russian = require('./locale/ru.json');
 
+// DEFINE READLINE INTERFACE
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+// GLOBAL VARIABLES
 const EOL = '\n';
+let targetNumber = null;
+let locale = null;
 
+// SET SYSTEM LOCALE IF AVAILABLE
+if (process.env.LANG.includes("ru")) locale = russian;
+else locale = english;
+
+// GET RANDOM VALUE FROM AN ARRAY
 const getRandomAnswer = function (array) {
     return array[Math.floor(Math.random() * Math.floor(array.length))];
 };
 
-let targetNumber = Math.floor(Math.random() * Math.floor(101));
+// READLINE QUESTION FUNCTION WITH NUMBER GUESS
+const number_question = function() {
+    rl.question("", (answer) => {
+        if (answer!=Number(answer)) rl.write(getRandomAnswer(locale.answer.invalid) + EOL);
+        if (answer>targetNumber) rl.write(getRandomAnswer(locale.answer.smaller) + EOL);
+        if (answer<targetNumber) rl.write(getRandomAnswer(locale.answer.bigger) + EOL);
+        if (answer==targetNumber) initialisation(getRandomAnswer(locale.answer.right) + EOL);
+        else number_question();
+    });
+};
 
-rl.question(english.introduction + EOL, (answer) => {
-    if (answer===english.key.yes) {
-        // rl.question(english.ready);
-        rl.close();
-    } else if (answer===english.key.no) {
-        rl.write(getRandomAnswer(english.exit) + EOL);
-        rl.close();
-    } else {
-        rl.close();
-    }
-})
+// NUMBER GENERATION
+const number_is_ready = function() {
+    targetNumber = Math.floor(Math.random() * Math.floor(101));
+    rl.write(locale.ready + EOL);
+    number_question();
+}
+
+// READLINE QUESTION FUNCTION IF PLAYER IS READY
+const initialisation = function(line) {
+    rl.question(line, (answer) => {
+        switch(answer) {
+            case locale.key.yes:
+                number_is_ready();
+                break;
+            case locale.key.no:
+                rl.write(getRandomAnswer(locale.exit) + EOL);
+                rl.close();
+                break;
+            default:
+                initialisation(locale.introduction + EOL);
+                break;
+        };
+    });
+};
+
+// GAME START
+initialisation(locale.introduction + EOL);
